d3dd0e83c6c26402a31903368d9755a6ff28e47b
Silas Duarte <silasduarte1@hotmail.com>
2023-05-26 18:50:51 -0300

    Feito organização produtos e carrinho

--- ecomerce/src/components/shoppingCart/cart/Cart.jsx
+++ ecomerce/src/components/shoppingCart/cart/Cart.jsx
@@ -2,11 +2,39 @@ import Item from "../items/Items"
 import { CartBox } from "./cartStyle"
 
 function Cart({ cart, amount, setCart, setAmount }) {
+
+    function removeItem(id) {
+        cart.map((produto) => {
+            if (produto.id === id && produto.quantidade >= 1) {
+                produto.quantidade = produto.quantidade - 1
+                setCart([...cart])
+                setAmount(amount - produto.value)
+            }
+            if (produto.quantidade === 0) {
+                const novoCarrinho = cart.filter(
+                    produtos => produtos.quantidade !== 0
+                )
+                setCart(novoCarrinho)
+            }
+        })
+    }
     return (
         <>
             <CartBox>
                 <p>Cart</p>
-                <Item />
+                {cart.map((product) => {
+                    return <Item
+                        key={product.id}
+                        nome={product.name}
+                        valor={(product.value * product.quantidade).toFixed(2).replace(".", ",")}
+                        quantidade={product.quantidade}
+                        imagem={product.imageUrl}
+                        id={product.id}
+                        removeItem={removeItem}
+                    />
+                })}
+                <p>Valor total: R$ {amount.toFixed(2).replace(".", ",")}</p>
+
             </CartBox>
         </>
     )
--- ecomerce/src/components/shoppingCart/items/Items.jsx
+++ ecomerce/src/components/shoppingCart/items/Items.jsx
@@ -1,6 +1,11 @@
-function Item () {
+function Item({ nome, valor, quantidade, imagem, id, removeItem }) {
     return (
-        <p>item</p>
+        <>
+            <p> Nome: {nome}</p>
+            <p>Valor: R$ {valor}</p>
+            <p>Quantidade: {quantidade}</p>
+            <button onClick={() => removeItem(id)}>Remover</button>
+        </ >
     )
 }
 export default Item 
